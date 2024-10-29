92cc786ecd4bece5b8b180beda937cc6a71fc241
Davi Luiz Candido Lima <Davi Lima>
2024-10-29 17:34:50 -0300

    hotfix: invalid SSL certificate

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class ProdConfig:
     MONGO_APP_NAME = os.getenv('MONGO_APP_NAME')
     MONGO_URL = os.getenv('MONGO_URL')
     MONGODB_SETTINGS = {
-        'host': 'mongodb+srv://627daviluiz:%s@comunidadedevops-python.xb0cw.mongodb.net/?retryWrites=true&w=majority&appName=comunidadedevops-python' %(MONGODB_PASSWORD)
+        'host': "mongodb+srv://627daviluiz:%s@comunidadedevops-python.xb0cw.mongodb.net/?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true&appName=comunidadedevops-python" %(MONGODB_PASSWORD)
 
     }
 
