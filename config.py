f1cce4af41349c6c55c02983c679665a3f320f26
Davi Luiz Candido Lima <Davi Lima>
2024-10-29 16:29:05 -0300

    feat: changing config for production environment

--- config.py
+++ config.py
@@ -12,6 +12,26 @@ class DevConfig():
     }
 
 
+class ProdConfig:
+    MONGODB_USER = os.getenv('MONGODB_USER')
+    MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
+    MONGODB_HOST = os.getenv('MONGODB_HOST')
+    MONGODB_DB = os.getenv('MONGODB_DB')
+    MONGO_APP_NAME = os.getenv('MONGO_APP_NAME')
+    MONGO_URL = os.getenv('MONGO_URL')
+    MONGODB_SETTINGS = {
+        'host': 'mongodb+srv://%s:%s@%s/%s?%s=%s' % (
+            MONGODB_USER,
+            MONGODB_PASSWORD,
+            MONGODB_HOST,
+            MONGODB_DB,
+            MONGO_APP_NAME,
+            MONGO_URL
+        )
+
+    }
+
+
 class MockConfig:
     MONGODB_SETTINGS = {
         'db': 'users',
